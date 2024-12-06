5f2b3723bbd6ddcfdc2cb10d06462cdad561462a
Pavel Tykachev <tykachev@byndyusoft.com>
2024-12-06 16:25:10 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # nest-logger
-Logger module for NestJS apps based on Pino logger 
+
+[![npm@latest](https://img.shields.io/npm/v/@byndyusoft/nest-logger/latest.svg)](https://www.npmjs.com/package/@byndyusoft/nest-logger)
+[![test](https://github.com/Byndyusoft/nest-logger/actions/workflows/test.yaml/badge.svg?branch=master)](https://github.com/Byndyusoft/nest-logger/actions/workflows/test.yaml)
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
+[![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
+
+Logger module for NestJS apps based on Pino logger
+
+## Requirements
+
+- Node.js v20 LTS or later
+- npm or yarn
+
+## Install
+
+```bash
+npm install @byndyusoft/nest-logger
+```
+
+or
+
+```bash
+yarn add @byndyusoft/nest-logger
+```
+
+## Usage
+
+```typescript
+import { LoggerModuleNew, LogLevel } from "./logger";
+
+LoggerModuleNew.forRoot({
+  base: {
+    configEnv: 'local',
+    name: 'root',
+    version: '0.0.0-development',
+  },
+  level: LogLevel.info,
+  pretty: true,
+}),
+```
+
+## Usage Async
+
+```typescript
+import { LoggerModuleNew } from "./logger";
+import { ConfigDto } from "./config";
+import { PackageJsonDto } from "./packageJson";
+
+LoggerModuleNew.forRootAsync({
+  inject: [ConfigDto, PackageJsonDto],
+  useFactory: (configDto: ConfigDto, packageJson: PackageJsonDto) => ({
+    base: {
+      configEnv: configDto.configEnv,
+      name: packageJson.name,
+      version: packageJson.version,
+    },
+    level: configDto.logger.level,
+    pretty: configDto.logger.pretty,
+
+  })
+}),
+```
+
+## Maintainers
+
+- [@Byndyusoft/owners](https://github.com/orgs/Byndyusoft/teams/owners) <<github.maintain@byndyusoft.com>>
+- [@Byndyusoft/team](https://github.com/orgs/Byndyusoft/teams/team)
+- [@tykachev](https://github.com/tykachev)
+
+## License
+
+This repository is released under version 2.0 of the
+[Apache License](https://www.apache.org/licenses/LICENSE-2.0).
---
+++ src/loggerModule.ts
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2024 Byndyusoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { TRegisterAsyncOptions } from "@byndyusoft/nest-dynamic-module";
+import { LoggerModule as PinoLoggerModule} from "@byndyusoft/nest-pino";
+import {
+  PinoHttpLoggerOptionsBuilder,
+  PinoLoggerFactory,
+  PinoLoggerOptionsBuilder,
+} from "@byndyusoft/pino-logger-factory";
+import { DynamicModule,Module } from "@nestjs/common";
+import { plainToClass } from "class-transformer";
+import { Options as PinoOptions } from 'pino-http';
+
+import {LoggerOptionsDto} from './dtos/loggerOptionsDto'
+
+// We need increase nest-pino LoggerModule topological level for correct middlewares register
+@Module({})
+
+export class LoggerModule {
+
+  public static forRoot(rawLoggerOptions: LoggerOptionsDto): DynamicModule {
+    const loggerOptions = plainToClass(LoggerOptionsDto, rawLoggerOptions);
+    return {
+      module: LoggerModule,
+      imports: [
+        PinoLoggerModule.forRoot({
+          pinoHttp: LoggerModule.getPinoOptions(loggerOptions),
+        }),
+      ],
+    };
+  }
+
+  public static forRootAsync(options: TRegisterAsyncOptions<LoggerOptionsDto>): DynamicModule {
+    return {
+      module: LoggerModule,
+      imports: [
+        PinoLoggerModule.forRootAsync({
+          inject: options.inject,
+          useFactory(...args: unknown[]) {
+            const rawLoggerOptions = options.useFactory?.(...args);
+            const loggerOptions = plainToClass(LoggerOptionsDto, rawLoggerOptions);
+            return {
+              pinoHttp: LoggerModule.getPinoOptions(loggerOptions),
+            }
+          },
+        }),
+      ],
+    };
+  }
+
+  private static getPinoOptions(loggerOptions: LoggerOptionsDto): PinoOptions {
+
+    const pinoLoggerOptions = new PinoLoggerOptionsBuilder()
+    if (loggerOptions.base) {
+      pinoLoggerOptions.withBase({
+        name: loggerOptions.base.name,
+        version: loggerOptions.base.version,
+        env: loggerOptions.base.configEnv,
+      })
+    }
+    if (loggerOptions.level !== undefined) {
+      pinoLoggerOptions.withLevel(loggerOptions.level)
+    }
+    if (loggerOptions.pretty) {
+      pinoLoggerOptions.withPrettyPrint(loggerOptions.pretty)
+    }
+
+    return new PinoHttpLoggerOptionsBuilder()
+    .withLogger(
+      new PinoLoggerFactory().create(
+        pinoLoggerOptions.build()
+      ),
+    )
+    .build()
+  }
+}
